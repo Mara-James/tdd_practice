db5eaacddb9caa7eddcbb670f9b35803375c814f
Mara Cunningham <mara.james.codes@gmail.com>
2023-07-28 13:00:21 -0500

    setting up virtual env

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cybele/programming/learning-tree/tdd_practice/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
